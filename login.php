59bed620224a432f5c5c2db9655b18c88287e9ae
Lưu Ly Nguyễn <nguyenluuly2k2@gmail.com>
2023-02-22 14:45:47 +0700

    set admin

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
             <div class="container-fluid">
                 <div class="my-logo">
                     <a class="navbar-brand" href="#">
-                        <img src="images/logo2.png" alt="" class="img-fluid">
+                        <img src="image/logo2.png" alt="" class="img-fluid">
                     </a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
