5fc3be18c041e1c88e54281bc27d134f85b981fc
VietDH <vietdtds@gmail.com>
2023-02-27 15:11:00 +0700

    update toi uu header va footer, utf8

--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 include 'header.php';
 require './connectDB.php';
 $conn = connectDB();
-$query = "SELECT * FROM `baiviet`;";
+$query = "SELECT * FROM `baiviet`";
 $result = mysqli_query($conn, $query);
 
 ?>
 
+<body>
+  <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./images/slideshow/slide01.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./images/slideshow/slide02.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./images/slideshow/slide03.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</body>
 <main class="container-fluid mt-3">
   <h3 class="text-center text-uppercase mt-3 mb-4 text-secondary fs-2">TOP bài hát yêu thích</h3>
   <div class="row gy-3">
@@ -15,7 +43,7 @@ $result = mysqli_query($conn, $query);
       while ($row = mysqli_fetch_assoc($result)) { ?>
         <div class="col-sm-3">
           <div class="card mb-2" id="card" style="width: 100%;">
-            <img src=<?= $row['hinhanh'] ?? "images/songs/cayvagio.jpg" ?> class="card-img-top" id="cardImg" alt="...">
+            <img src=<?= $row['hinhanh'] ?? "images/songs/cayvagio.jpg" ?> class="card-img-top" id="cardImg" alt="Hình ảnh minh họa">
             <div class="card-body">
               <h5 class="card-title text-center">
                 <a href="./detail.php?ma_bviet=<?= $row['ma_bviet'] ?>" class="text-decoration-none text-dark" id="cardTitle">
